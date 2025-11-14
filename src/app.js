d5d3db1917bf0775fa3b2c20f92d88868eb0d5c5
Tejas Shinde <tejasshinde@Tejass-MacBook-Pro.local>
2025-11-14 23:40:43 +0530

    backend fixes

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 const authRouter = require("./routes/auth");
-const mlRouter = require("./routes/ml"); // NEW
+const mlRouter = require("./routes/ml");
+const patientRouter = require("./routes/patient");
 const connectDB = require("./config/database");
+const auth = require("./middlewares/auth");
 
 // Middlewares
 app.use(cors());
@@ -14,22 +16,22 @@ app.use(cookieParser());
 
 // Routes
 app.use("/auth", authRouter);
-app.use("/ml", mlRouter); // NEW ML ROUTE
-
+app.use("/ml", mlRouter);
+app.use("/", authRouter);
+app.use("/patient", auth, patientRouter);
 app.get("/test", (req, res) => {
-res.send("Server is running on PORT 7777");
+  res.send("Server is running on PORT 7777");
 });
 
 connectDB()
-.then(() => {
-console.log("✅ Database connection Established.");
-app.listen(7777, () => {
-console.log("✅ Node.js server listening on port 7777");
-console.log(" - Main API: http://localhost:7777");
-console.log(" - ML API (Flask): http://localhost:5000");
-});
-})
-.catch((err) => {
-console.log("❌ Database Connection Failed..." + err);
-});
-
+  .then(() => {
+    console.log("✅ Database connection Established.");
+    app.listen(7777, () => {
+      console.log("✅ Node.js server listening on port 7777");
+      console.log(" - Main API: http://localhost:7777");
+      console.log(" - ML API (Flask): http://localhost:5000");
+    });
+  })
+  .catch((err) => {
+    console.log("❌ Database Connection Failed..." + err);
+  });
